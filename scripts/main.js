ac16465943151f2cb72ba56741646294f2618ad8
Jason Yu <jasonyu@cs.utexas.edu>
2019-11-09 19:09:32 -0600

    Added basic canvas

--- scripts/main.js
+++ scripts/main.js
@@ -27,3 +27,16 @@ if (localStorage.getItem('name')) {
     let storedName = localStorage.getItem('name');
     myHeading.textContent = 'Heading 1 / ' + storedName;
 }
+
+function draw() {
+    var canvas = document.getElementById('tutorial');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(30, 30, 50, 50);
+    }
+}
